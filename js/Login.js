d228f7a6fe470d1d21e6ca5c72958bbfbccfa872
kyun91 <31693827+kyun91@users.noreply.github.com>
2017-09-11 22:44:31 +0900

    login upload

---
+++ js/Login.js
@@ -0,0 +1,25 @@
+var $userLogin = null;
+$(document).ready(function(){
+		init();
+
+});
+
+function init(){
+	$userLogin = $(".userLogin");
+	$userLogin.click(function(){
+			alert("로그인 정보가 옳바르지 않습니다. Guest로 접속합니다.");
+			window.open("index.html","_parent");
+	});
+}
+
+
+
+//
+// function createAccount(){
+//            window.open("createAccount1.html","_parent");
+//
+// }
+//
+// function findInfo(){
+//              window.open("findInfo.html","_parent");
+// }
